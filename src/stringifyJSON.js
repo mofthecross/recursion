743bf35de4a91e910677396a466732df72ee7ca2
mofthecross <mofthecross@gmail.com>
2016-05-06 18:18:56 -0700

    Impletment stringifyJSON but needs refactoring.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,29 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+//
+// var stringifyJSON = function(obj) {
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  var stringItOn = {
+    string: function() { return '"' + obj + '"';},
+    number: function() { return obj + "";},
+    boolean: function() { return obj + "";},
+    object: function() {
+      if (Array.isArray(obj)) {
+        return "[" + obj.map(function(item) {
+          return stringifyJSON(item);
+        }) + "]";
+      } else if (obj === null) { return "null";
+      } else {
+        var results = [];
+        for (var key in obj) {
+          if (typeof obj[key] !== 'function'&& obj[key] !== undefined) {
+            results.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+          }
+        }
+        return '{' + results + '}';
+      }
+    }
+  }
+  return stringItOn[typeof obj](obj);
+ };
